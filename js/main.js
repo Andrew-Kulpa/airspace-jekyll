6ef857e2da6f717e49ca18325ed52ff90bfbbe3d
Andrew Kulpa <aj-kulpa@wiu.edu>
2019-03-06 11:29:53 -0600

    remove carousel, added language and framework logos

--- js/main.js
+++ js/main.js
@@ -4,28 +4,28 @@ $(document).ready(function(){
 	$("#portfolio-content-active").mixItUp();
 
 
-	$("#testimonial-slider").owlCarousel({
-	    paginationSpeed : 500,      
-	    singleItem:true,
-	    autoPlay: 3000,
-	});
+	// $("#testimonial-slider").owlCarousel({
+	//     paginationSpeed : 500,      
+	//     singleItem:true,
+	//     autoPlay: 3000,
+	// });
 
 
 
 
-	$("#clients-logo").owlCarousel({
-		autoPlay: 3000,
-		items : 5,
-		itemsDesktop : [1199,5],
-		itemsDesktopSmall : [979,5],
-	});
+	// $("#clients-logo").owlCarousel({
+	// 	autoPlay: 3000,
+	// 	items : 5,
+	// 	itemsDesktop : [1199,5],
+	// 	itemsDesktopSmall : [979,5],
+	// });
 
-	$("#works-logo").owlCarousel({
-		autoPlay: 3000,
-		items : 5,
-		itemsDesktop : [1199,5],
-		itemsDesktopSmall : [979,5],
-	});
+	// $("#works-logo").owlCarousel({
+	// 	autoPlay: 3000,
+	// 	items : 5,
+	// 	itemsDesktop : [1199,5],
+	// 	itemsDesktopSmall : [979,5],
+	// });
 
 
 	// google map
